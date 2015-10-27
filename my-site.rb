e311a5314583a4ef4a440cf1c0a67e761496ed57
Tammy <misstamar@gmail.com>
2015-10-27 14:59:55 -0700

    I think I finish

--- my-site.rb
+++ my-site.rb
@@ -2,7 +2,20 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
-  get "/" do
+  get "/:name" do
+    "Hello #{params[:name]}"
+  end
 
+  get "/hello.html" do
+    "Welcome to the INTERNET"
+#http://localhost:9292/hello.html
+ end
+
+  get "/candies" do
+    File.read('candies.html')
+  end
+
+  get "/Stickers" do
+    File.read('Stickers.html')
   end
 end
