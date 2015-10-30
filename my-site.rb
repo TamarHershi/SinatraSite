c870935ce9a15b601f6abb8e797ededf677f480c
Tammy <misstamar@gmail.com>
2015-10-30 13:36:54 -0700

    made a layout and it works

--- my-site.rb
+++ my-site.rb
@@ -7,22 +7,31 @@ class MySite < Sinatra::Base
   end
 
   get "/candies" do
+    @h1 = "CANDIES!!!"
     @candies = ["white chocolate", "gummy bear", "snickers", "cotton candy", "pink gum"]
     erb :candies
   end
 
   get "/stickers" do
+    @h1 = "Emily Loves Stickers!"
     erb :stickers
   end
 
   get "/about" do
+    @h1 = "about"
     @year = Time.new.year
     @bday = Time.new(1984, 9, 28)
     erb :about
   end
 
   get "/projects" do
+    @h1 = "my projects"
     erb :projects
+  end
+
+  get "/index" do
+    @h1 ="My Home Page"
+    erb :index
   end
 
 end
